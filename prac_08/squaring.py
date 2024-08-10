a02a9f79f3d7e8aff627fccb7ae60efa0a95c3a4
Akram Kasozi <akram.kasozi@my.jcu.edu.au>
2024-08-10 21:55:16 +1000

    add practical 08 files

---
+++ prac_08/squaring.py
@@ -0,0 +1,32 @@
+"""
+CP1404Practica l
+Kivy GUI program to square a number
+Started 10/18/2024
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+__created__ = 'Akram Kasozi'
+
+
+class SquareNumberApp(App):
+    """ SquareNumberApp is a Kivy App for squaring a number """
+    def build(self):
+        """ build the Kivy app from the kv file """
+        Window.size = (200, 100)
+        self.title = "Square Number"
+        self.root = Builder.load_file('squaring.kv')
+        return self.root
+
+    def handle_calculate(self, value):
+        """ handle calculation (could be button press or other call), output result to label widget """
+        try:
+            result = int(value) ** 2  # Perform conversion and calculation here
+            self.root.ids.output_label.text = str(result)
+        except ValueError:
+            self.root.ids.output_label.text = "Invalid input"
+
+
+SquareNumberApp().run()
